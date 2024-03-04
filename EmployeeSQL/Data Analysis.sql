a4b18891d92586dff29971cbb1964ca3deee34ec
mjsdupont <152733829+mjsdupont@users.noreply.github.com>
2024-03-04 15:33:26 -0500

    Final push

---
+++ EmployeeSQL/Data Analysis.sql
@@ -0,0 +1,39 @@
+-- Active: 1709584153779@@127.0.0.1@3306
+SELECT emp_no, last_name, first_name, sex, salary
+FROM employees
+JOIN salaries ON employees.emp_no = salaries.emp_no;
+
+SELECT first_name, last_name, hire_date
+FROM employees
+WHERE hire_date BETWEEN '1986-01-01' AND '1986-12-31';
+
+SELECT dm.dept_no, d.dept_name, dm.emp_no, e.last_name, e.first_name
+FROM dept_manager dm
+JOIN departments d ON dm.dept_no = d.dept_no
+JOIN employees e ON dm.emp_no = e.emp_no;
+
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM employees e
+JOIN dept_emp de ON e.emp_no = de.emp_no
+JOIN departments d ON de.dept_no = d.dept_no;
+
+SELECT first_name, last_name, sex
+FROM employees
+WHERE first_name = 'Hercules' AND last_name LIKE 'B%';
+
+SELECT e.emp_no, e.last_name, e.first_name
+FROM employees e
+JOIN dept_emp de ON e.emp_no = de.emp_no
+JOIN departments d ON de.dept_no = d.dept_no
+WHERE d.dept_name = 'Sales';
+
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM employees e
+JOIN dept_emp de ON e.emp_no = de.emp_no
+JOIN departments d ON de.dept_no = d.dept_no
+WHERE d.dept_name IN ('Sales', 'Development');
+
+SELECT last_name, COUNT(*) AS frequency
+FROM employees
+GROUP BY last_name
+ORDER BY frequency DESC;
---
+++ EmployeeSQL/Data Engineering.sql
@@ -0,0 +1,44 @@
+CREATE TABLE departments (
+    dept_no VARCHAR(4) PRIMARY KEY,
+    dept_name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE employees (
+    emp_no INT PRIMARY KEY,
+    emp_title_id VARCHAR(10) NOT NULL,
+    birth_date DATE NOT NULL,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    sex CHAR(1) NOT NULL,
+    hire_date DATE NOT NULL,
+    dept_no VARCHAR(4),
+    FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+);
+
+CREATE TABLE titles (
+    title_id VARCHAR(10) PRIMARY KEY,
+    title VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE salaries (
+    emp_no INT,
+    salary INT NOT NULL,
+    PRIMARY KEY (emp_no, salary), -- Composite primary key
+    FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
+
+CREATE TABLE dept_manager (
+    dept_no VARCHAR(4),
+    emp_no INT,
+    PRIMARY KEY (dept_no, emp_no), -- Composite primary key
+    FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+    FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
+
+CREATE TABLE dept_emp (
+    emp_no INT,
+    dept_no VARCHAR(4),
+    PRIMARY KEY (emp_no, dept_no), -- Composite primary key
+    FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+    FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+);
